24c919cb6c33f1596ffd7bf7bf143acf5bda7bc6
Pavan Kesava Rao <pkesavar@redhat.com>
2023-09-06 10:02:54 -0400

    Add Backup and Restore tests for AAP on GCP
    This commit adds the following:
    - new methods to AoC GCP Backup lib package

--- lib/aoc/gcp/operations/backup.py
+++ lib/aoc/gcp/operations/backup.py
@@ -14,25 +14,48 @@ from lib.aoc.ops_container import OpsContainer
 __all__ = [
     "AocGcpBackup",
     "AocGcpBackupDataVars",
+    "AocGcpBackupDataExtraVars",
+    "AocGcpBackupStackResult",
 ]
 
 
-class AocGcpBackUpDataExtraVars(TypedDict, total=False):
+class AocGcpBackupDataExtraVars(TypedDict, total=False):
     """AoC default backup operations playbook data extra vars."""
 
-    # TODO: Implement typed dict
-    todo: str
+    gcp_compute_region: str
+    gcp_compute_zone: str
+    gcp_storage_bucket: str
+    gcp_backup_prefix: str
 
 
 class AocGcpBackupDataVars(TypedDict, total=False):
     """AoC default backup operations playbook data vars."""
 
-    # TODO: Implement typed dict
-    todo: str
+    cloud_credentials_path: str
+    deployment_name: str
+    extra_vars: AocGcpBackupDataExtraVars
+
+
+class AocGcpBackupStackResult(TypedDict):
+    """Aoc Gcp stack backup results."""
+
+    playbook_output: str
+    playbook_result: bool
+    backup_object_name: str
 
 
 class AocGcpBackup(OpsContainer):
-    """AocGcpBackup Class."""
+    """AocGcpBackup class.
+
+    This class handles all operations to perform an aoc on gcp stack backup.
+    Perform the following to initiate a backup:
+        1. Instantiate the class constructing an object
+            > aoc_gcp_backup = AocGcpBackup(...)
+        2. Call the `setup` method to perform pre backup operations
+            > aoc_gcp_backup.setup()
+        3. Call the `backup_stack` method to perform backup
+            > aoc_gcp_backup.backup_stack()
+    """
 
     def __init__(
         self,
@@ -66,19 +89,51 @@ class AocGcpBackup(OpsContainer):
             ansible_module,
         )
 
+        self.ansible_module: BaseHostManager = ansible_module
+
         self.command_generator_vars: AocGcpBackupDataVars = command_generator_vars
-        self.command_generator_setup()
 
     def command_generator_setup(self) -> None:
         """Performs any setup required to run command generator playbooks."""
-        # TODO: Implementation
-        self.command = "command_generator_vars"
+        self.command_args: List[str] = [
+            f'gcp_foundation_stack_name={self.command_generator_vars["deployment_name"]}',
+            f'gcp_region={self.command_generator_vars["extra_vars"]["gcp_compute_region"]}',
+            f'gcp_zone={self.command_generator_vars["extra_vars"]["gcp_compute_zone"]}',
+            f'gcp_storage_bucket={self.command_generator_vars["extra_vars"]["gcp_storage_bucket"]}',
+        ]
+        cloud_credentials_path: str
 
-    def validate(self) -> bool:
-        """Validates any necessary input prior to performing backups.
+        if self.aoc_version != "2.3":
+            self.env_vars = {
+                "DEPLOYMENT_NAME": f'{self.command_generator_vars["deployment_name"]}',
+                "GENERATE_INVENTORY": "true",
+            }
+            self.command_args.extend(
+                [
+                    f'gcp_backup_prefix={self.command_generator_vars["extra_vars"]["gcp_backup_prefix"]}',
+                ]
+            )
 
-        :return: the overall result of the validations performed
-        """
-        return self.validate_command_generator_vars(
-            typing.cast(Dict[str, str], self.command_generator_vars)
-        )
+        self.command = "redhat.ansible_on_clouds.gcp_backup_deployment"
+        self.env_vars.update({
+            "ANSIBLE_CONFIG": "../gcp-ansible.cfg",
+            "PLATFORM": f"{self.cloud.upper()}",
+        })
+        self.volume_mounts = [
+            f'{self.command_generator_vars["cloud_credentials_path"]}:/home/runner/.gcp/credentials:ro',
+        ]
+
+    def create_storage_bucket(self) -> bool:
+        """Create gcp storage bucket to store backup files."""
+        # TODO: Implement gcp_storage_bucket module to create bucket
+        pass
+
+    def delete_storage_bucket(self) -> bool:
+        """Delete gcp storage bucket holding backup files."""
+        # TODO: Implement gcp_storage_bucket module to delete bucket
+        pass
+
+    def get_storage_backup_object(self) -> str:
+        """Gets the stack backup object stored in the gcp bucket."""
+        # TODO: Implement gcp_storage_bucket module to fetch backup objects
+        pass
